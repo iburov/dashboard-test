93d51a362411522f777c9668a5d2149527364263
iburov.ca <iburov.ca@gmail.com>
2020-05-24 15:55:46 -0400

    add @angular/material to the project

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ErrorComponent } from './error/error.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, ErrorComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
