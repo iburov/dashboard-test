9ea8d11c2d7d2b8a1a2d22b34562d8109cca7cb8
iburov.ca <iburov.ca@gmail.com>
2020-05-24 22:46:58 -0400

    make schedule page work

--- react/src/components/Schedule/Schedule.jsx
+++ react/src/components/Schedule/Schedule.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
 import "./Schedule.scss";
 
 function Schedule() {
-  return <div className="dashboard-schedule">schedule</div>;
+  return (
+    <div className="app-schedule">
+      <Calendar />
+    </div>
+  );
 }
 
 export default Schedule;
