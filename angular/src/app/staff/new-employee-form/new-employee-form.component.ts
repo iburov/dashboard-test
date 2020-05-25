e7dd7732e203f85c790d8928988027134e40964b
iburov.ca <iburov.ca@gmail.com>
2020-05-24 21:13:16 -0400

    make new employee form work

--- angular/src/app/staff/new-employee-form/new-employee-form.component.ts
+++ angular/src/app/staff/new-employee-form/new-employee-form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { StaffService } from '../staff.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Employee, EmployeeGroup } from '../staff.model';
 @Component({
   selector: 'app-new-employee-form',
   templateUrl: './new-employee-form.component.html',
-  styleUrls: ['./new-employee-form.component.scss']
+  styleUrls: ['./new-employee-form.component.scss'],
 })
 export class NewEmployeeFormComponent implements OnInit {
+  firstName: string;
+  lastName: string;
+  group: EmployeeGroup;
 
-  constructor() { }
+  constructor(
+    private staffService: StaffService,
+    public dialogRef: MatDialogRef<NewEmployeeFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Employee
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  create() {
+    this.staffService.addEmployee({
+      id: new Date().getTime(),
+      firstName: this.firstName,
+      lastName: this.lastName,
+      group: this.group,
+    });
+
+    this.dialogRef.close();
   }
 
+  cancel() {
+    this.dialogRef.close();
+  }
+
+  // TODO: add validation!!!
 }
