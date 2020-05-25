d22daee737b3ff829444fa7aa92d1715de9342a6
iburov.ca <iburov.ca@gmail.com>
2020-05-24 22:37:31 -0400

    remove navButton

--- react/src/components/Nav/Nav.jsx
+++ react/src/components/Nav/Nav.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Nav.scss";
 import { NavLink } from "react-router-dom";
-import NavButton from "./NavButton/NavButton.jsx";
 
 function Nav() {
   const navData = [
@@ -10,7 +9,6 @@ function Nav() {
   ];
 
   let navButtonElements = navData.map((navButton, i) => (
-    // <NavButton key={i} value={navButton.value} link={navButton.link} />
     <NavLink to={navButton.link} className="nav-link">
       {navButton.value}
     </NavLink>
