d9e4d68ab49b610b103db9d4cbc11e8af32478f6
iburov.ca <iburov.ca@gmail.com>
2020-05-24 22:36:59 -0400

    make page not found page work

--- react/src/components/PageNotFound/PageNotFound.jsx
+++ react/src/components/PageNotFound/PageNotFound.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./PageNotFound.scss";
 
 function PageNotFound() {
-  return <div className="dashboard-page-not-found">404 - PageNotFound</div>;
+  return (
+    <div className="app-page-not-found">
+      <h1>Something went wrong</h1>
+
+      <p>
+        There seems to be an issue with the link that you are trying to use, the
+        link may be incorrect or it needs to be updated.
+      </p>
+
+      <img
+        src="https://thumbs.gfycat.com/UnhealthyLimpEyelashpitviper-max-1mb.gif"
+        alt="Page Not Found"
+      />
+    </div>
+  );
 }
 
 export default PageNotFound;
