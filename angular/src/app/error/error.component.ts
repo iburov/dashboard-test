1d2e7f7e71f46583a12de58e643460d156507822
iburov.ca <iburov.ca@gmail.com>
2020-05-24 22:29:19 -0400

    make error page work

--- angular/src/app/error/error.component.ts
+++ angular/src/app/error/error.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-error',
+  selector: 'dashboard-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
+  styleUrls: ['./error.component.scss'],
 })
 export class ErrorComponent implements OnInit {
+  errorId$: Observable<string>;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.errorId$ = this.activatedRoute.params.pipe(
+      map((params) => params.errorId)
+    );
   }
-
 }
